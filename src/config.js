ef7ae40a5005b93e1803ad52ac368e5fa2aa927e
katiecannington <katiecannington@egpgrow.com>
2026-01-26 20:23:39 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -66,7 +66,7 @@ export const config = {
   team: [
     {
       name: "Rev. Kevin Wimbish, MS, LMFT",
-      role: "Founder",
+      role: "Co-Founder",
       specialty: "Licensed Marriage & Family Therapist (NC, TN)",
       image: "/kevin_picture.png"
     },
